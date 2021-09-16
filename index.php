2a4ed706bed943a9a84692a9fd15baab7a089868
piotrroae <roae@poczta.fm>
2021-09-16 13:51:11 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Wprowadzenie do PHP</title>
+  </head>
+  <body>
+    <?php
+      $serwer = "localhost";
+      $user = "root";
+      $password ="";
+      $baza = "wprowadzeniephp";
+
+      $mysqli = new mysqli($serwer, $user, $password, $baza);
+
+      //sprawdzenie połączenia:
+      if ($mysqli -> connect_errno) {
+        echo "Nie udało się połączyć z MySQL: ".$mysqli->connect_error;
+        exit();
+      }
+
+      $sql = "SELECT id, imie, nazwisko, stanowisko FROM pracownicy";
+      $wynik = $mysqli->query($sql);
+
+
+      ?>
+
+      <h1>Nauczyciele</h1>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>Imię</th>
+          <th>Nazwisko</th>
+          <th>Stanowisko</th>
+        </tr>
+
+        <?php
+        if ($wynik->num_rows > 0){
+          while ($rekord = $wynik->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>".$rekord["id"]."</td>";
+            echo "<td>".$rekord["imie"]."</td>";
+            echo "<td>".$rekord["nazwisko"]."</td>";
+            echo "<td>".$rekord["stanowisko"]."</td>";
+            echo "</tr>";
+          }
+
+        } else {echo "brak wyników";}
+
+        ?>
+      </table>
+
+
+
+
+      <?php $mysqli->close(); ?>
+
+  </body>
+</html>
